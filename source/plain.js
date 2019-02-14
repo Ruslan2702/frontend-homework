2590d881c52134eb631738036fc631846390c4c4
Ruslan Shakhaev <ruslan-shahaev@mail.ru>
2019-02-14 13:40:50 +0300

    Arrow functions

--- source/plain.js
+++ source/plain.js
@@ -1,14 +1,4 @@
 'use strict';
 
-const plain = function (arr) {
-    if (!Array.isArray(arr)) {
-        return null;
-    }
+let plain = (arr) => { if (!Array.isArray(arr)) { return null; }  return arr.reduce((seq, current) => { return seq.concat(Array.isArray(current) ? plain(current) : current); }, []); };
 
-    let result = [];
-    arr.forEach(function (element) {
-        result = result.concat(Array.isArray(element) ? plain(element) : element);
-    });
-
-    return result;
-};
