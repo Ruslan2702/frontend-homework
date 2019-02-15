a40631f3bd06dd209b7109b52d6b6d902fd3a3fd
Ruslan Shakhaev <ruslan-shahaev@mail.ru>
2019-02-15 19:41:20 +0300

    Formated arrow functions

--- source/plain.js
+++ source/plain.js
@@ -1,11 +1,11 @@
 'use strict';
 
-let plain = (arr) => {
+const plain = (arr) => {
     if (!Array.isArray(arr)) {
         return null;
     }
 
     return arr.reduce((seq, current) => {
         return seq.concat(Array.isArray(current) ? plain(current) : current);
-        }, []);
+    }, []);
 };
